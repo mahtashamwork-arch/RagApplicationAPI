5ef58fda1ff900939c670ddc8d3120148f3cbdf9
ahtasham <muhammad.ahtasham@pgrninc.com>
2025-11-27 16:44:30 +0500

    update

---
+++ Controllers/FileUploadController.cs
@@ -0,0 +1,30 @@
+﻿using Azure.Storage.Blobs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace firstRagWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileUploadController : ControllerBase
+    {
+        public readonly IConfiguration _config;
+        public FileUploadController(IConfiguration config)
+        {
+            _config = config;
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> upload(IFormFile file)
+        {
+            var blobclient = new BlobContainerClient(_config["Azure:BlobStorageConnectionString"], _config["Azure:BlobContainer"]);
+            await blobclient.CreateIfNotExistsAsync();
+
+            var blob = blobclient.GetBlobClient(file.FileName);
+            await using var stream = file.OpenReadStream();
+            await blob.UploadAsync(stream,overwrite:true);
+            return Ok(new {file.FileName});
+        }
+    }
+}
